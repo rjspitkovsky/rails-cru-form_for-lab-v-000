6f422b865f54e6d8e4764d6a6e6f805016a899f6
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-09 18:34:18 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,31 @@
 class GenresController < ApplicationController
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(post_params(:name, :bio))
+    @genre.save
+    redirect_to genre_path(@genre)
+
+    
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(post_params(:name, :bio))
+    redirect_to genre_path(@genre)
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def post_params(*args)
+    params.require(:genre).permit(*args)
+  end
+  
 end 
