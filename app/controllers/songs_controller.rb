92b2d01fb9b2f4f5bb4036651963ceb52dca0b5d
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-09 18:46:06 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,6 +4,14 @@ class SongsController < ApplicationController
     @song = Song.find(params[:id])
   end 
 
+  def create 
+    @artist = Artist.find_by(name: params[:artist])
+    @genre = Genre.find_by(name: params[:genre])
+    @song = Song.new(post_params(:name, @artist.id, @genre.id))
+    @song.save 
+    redirect_to song_path(@song)
+  end 
+
 
   def show
     @song = Song.find(params[:id])
