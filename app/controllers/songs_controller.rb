98019240e82bb138b00bc6b8eeebff0bf0d1f981
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-09 18:53:12 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,7 +2,7 @@ class SongsController < ApplicationController
 
   def index
     @songs = Song.all
-  end 
+  end
 
   def new
     @song = Song.find(params[:id])
@@ -26,10 +26,10 @@ class SongsController < ApplicationController
   end
 
   def update
-    @artist = Artist.find_by(name: params[:artist])
-    @genre = Genre.find_by(name: params[:genre])
+    # @artist = Artist.find_by(name: params[:artist])
+    # @genre = Genre.find_by(name: params[:genre])
     @song = Song.find(params[:id])
-    @song.update(post_params(:name, @artist.id, @genre.id))
+    @song.update(post_params(:name, :artist_id, :genre_id))
     redirect_to song_path(@song)
   end
 
