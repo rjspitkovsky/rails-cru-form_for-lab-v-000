7685279a2b2b3dc1c97082611649e86b1dfe28fb
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-09 18:25:41 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,5 +2,17 @@ class ArtistsController < ApplicationController
 
   def show
     @artist = Artist.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
   end 
 end
